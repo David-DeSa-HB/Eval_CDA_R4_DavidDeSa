536c3156d4622f9f49b8cd8ecf64a52afc08f314
David De Sa <david.desa707@gmail.com>
2024-10-29 11:17:54 +0100

    validation page

--- js/cart.js
+++ js/cart.js
@@ -1,25 +1,38 @@
 function displayCart(data) {
   element = document.getElementById("app");
+  console.log(cartList);
   element.innerHTML = `
           <h2>Panier</h2>
 
           ${cartList
             .map((dataCart) => {
               const product = dataCart.product;
-              // const quantity = cartList.filter((id) => id == product.id).length;
               return `
             <div>
               <h3>${product.nom_produit}</h3>
               <p>Prix: ${product.prix}</p>
               <p>Nombre: x${dataCart.number}</p>
               <img src="${product.image}" alt="${product.nom_produit}">
-              <button class="removeButton" data-id="${product.id}">Je n'en veux plus !</button>
-              <button class="addButton" data-id="${product.id}">J'en veux encore plus !</button>
+              <button class="substractButton" data-id="${product.id}">J'en veux moins !</button>
+              <button class="addButton" data-id="${product.id}">J'en veux plus !</button>
+              <button class="removeButton" data-id="${product.id}">Je ne veux plus de ça !</button>
             </div>`;
             })
             .join("")}
 
-            <button onclick="goBack()">Reviens !</button>`;
+            <button onclick="goBack()">Reviens !</button>
+            
+            ${cartList.length >0 ? `
+              <button onclick="showValidationPage()">Je veux payer maintenant !</button>` : ``}`;
+
+  element.querySelectorAll(".substractButton").forEach((button) => {
+    button.addEventListener("click", function (event) {
+      const productId = event.currentTarget.getAttribute("data-id");
+      const product = data.filter((product) => product.id === productId * 1)[0];
+      substractFromCart(product);
+      displayCart(data);
+    });
+  });
 
   element.querySelectorAll(".removeButton").forEach((button) => {
     button.addEventListener("click", function (event) {
@@ -38,13 +51,17 @@ function displayCart(data) {
       displayCart(data);
     });
   });
-}
 
-function removeFromCart(product) {
+function substractFromCart(product) {
   const myList = cartList.filter((data) => data.product.id === product.id);
   if (myList[0].number > 1) {
     myList[0].number--;
   } else {
-    cartList = cartList.filter((productCart) => product.id != productCart.product.id);
+    removeFromCart(product);
   }
+}
+
+function removeFromCart(product) {
+    cartList = cartList.filter((productCart) => product.id != productCart.product.id);
+}
 }
---
+++ js/validation.js
@@ -0,0 +1,77 @@
+function showValidationPage(){
+  document.title = "validation"
+
+  const app = document.getElementById("app");
+  app.innerHTML = "";
+
+  const element = document.createElement("div");
+  element.innerHTML = `
+  <div class="bground">
+  <div class="content">
+      <span class="close"></span>
+      <div class="modal-body">
+          <form name="reserve" action="index.html" method="get" id="form-inscription">
+              <div class="formData">
+                  <label>Prénom</label>
+                  <br />
+                  <input
+                      class="text-control"
+                      type="text"
+                      id="first"
+                      name="first"
+                      minlength="2" />
+                  <br />
+              </div>
+              <div class="formData">
+                  <label>Nom</label>
+                  <br />
+                  <input class="text-control" type="text" id="last" name="last" />
+                  <br />
+              </div>
+              <div class="formData">
+                  <label>E-mail</label>
+                  <br />
+                  <input class="text-control" type="email" id="email" name="email" />
+                  <br />
+              </div>
+              <div class="formData">
+                  <label>Date de naissance</label>
+                  <br />
+                  <input
+                      class="text-control"
+                      type="date"
+                      id="birthdate"
+                      name="birthdate" />
+                  <br />
+              </div>
+
+              <div class="formData">
+                  <input
+                      class="checkbox-input"
+                      type="checkbox"
+                      id="checkbox1"
+                      required />
+                  <label class="checkbox2-label" for="checkbox1" required>
+                      <span class="checkbox-icon"></span>
+                      J'ai lu et accepté les conditions de vente.
+                  </label>
+                  <br />
+                  <input class="checkbox-input" type="checkbox" id="checkbox2" />
+                  <label class="checkbox2-label" for="checkbox2">
+                      <span class="checkbox-icon"></span>
+                      Je souhaite être prévenu des prochains évènements.
+                  </label>
+                  <br />
+              </div>
+              <input
+                  class="btn-submit"
+                  type="submit"
+                  class="button"
+                  value="Je paye" />
+          </form>
+      </div>
+  </div>
+  </div>
+  `
+  app.appendChild(element);
+}
